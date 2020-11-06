bc9180c5aa1bae9a9938cafc1986cd66cb052749
FlorianLz <florianlaignez@gmail.com>
2020-11-06 10:56:42 +0100

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+
+
+// Compilation du SCSS en CSS
+
+function style (){
+    return gulp.src('./assets/scss/*.scss')
+
+        .pipe(sass())
+
+        .pipe(gulp.dest('./assets/css'))
+
+        .pipe(browserSync.stream());
+}
+
+function watch(){
+    browserSync.init({
+        server: {
+          baseDir: "./"
+        }
+      });
+    gulp.watch('./assets/scss/**/*.scss', style);
+    gulp.watch('./*.html').on('change', browserSync.reload);
+    gulp.watch('./assets/js/*.js').on('change', browserSync.reload);
+}
+
+
+exports.style = style;
+exports.watch = watch;
